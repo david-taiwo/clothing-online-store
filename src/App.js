ba5ec632f9a8f930b5736429671ce2b4d2233cfd
David Taiwo <davidtaiwo@gmail.com>
2023-04-04 06:39:34 +0100

    i have broken the homepage into smaller components and completed the styling the menu-item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react'
+import HomePage from './pages/homepage.component.jsx'
 import './App.css';
 
 function App() {
   return (
     <div>
-     
+     <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.css'
+import Directory from '../components/directory/directory.component.jsx'
+
+const HomePage = () => {
+  return (
+    <div className="homePage">     
+        <Directory />
+    </div>
+  )
+}
+
+export default HomePage
